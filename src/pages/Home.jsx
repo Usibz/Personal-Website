425715fb45a2cbd9c626b7121fd120c72dd4a7a2
Usibz <mir.yousof2000@gmail.com>
2025-07-26 01:55:33 +0330

    [feat]: contact section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { ContactSection } from "../components/ContactSection";
 import { Navbar } from "../components/Navbar";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
@@ -15,8 +16,7 @@ export const Home = () => {
       
       {/* Main Content */}
       <main>
-        
-
+        <ContactSection />
       </main>
 
       {/* Footer */}
