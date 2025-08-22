08f599e8423c2f2a7c98643c4c9c040a4c698c98
Usibz <mir.yousof2000@gmail.com>
2025-08-22 19:00:07 +0330

    [fix]: chainging the basename in App.jsx router again

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { NotFound } from "./pages/NotFound";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/Personal-Website">
         <Routes>
           <Route index element={<Home/>} />
           <Route path="*" element={<NotFound />} />
